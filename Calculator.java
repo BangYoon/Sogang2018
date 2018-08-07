e2672a3f2d997658e3299089169030ea6199d884
DESKTOP-RDETD6B\방윤 <yoonb720@naver.com>
2018-08-07 14:57:53 +0900

    minus 함수 추가

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,20 @@
 package com.mycompany.test;
 
   public class Calculator{
+
     public static void main(String[] args){
       int a=4;
       int b=2;
       System.out.printIn(sum(a,b));
+      System.out.printIn(minus(a,b));
     }
 
 
   private static int sum(int a, int b){
     return a+b;
+  }
+
+  private static int minus(int a, int b){
+    return a-b;
   }
 }
